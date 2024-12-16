d9f5ee535e5bdfcdfb85ee0019a3c80bec830359
knewlin713 <kaley_newlin@brown.edu>
2024-12-16 07:56:52 -0500

    merged router and mapbox

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,7 +2,6 @@ import { SetStateAction, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './styles/App.css'
-import Mapbox from './MapBox'
 import Dropdowns from './Dropdowns'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import About from './About'
@@ -10,10 +9,7 @@ import Home from './Home'
 import Navbar from './Navbar'
 
 
-function App() {  //used prop.clickedDorm[] to get clicked dorm
-  const [filteredDorms, setFilteredDorms] = useState<Array<string>>([]);
-  const [clickedDorm, setClickedDorms] = useState<string>('');
-  //set filtered dorms to change map highlights.
+function App() {
   return (
     <BrowserRouter>
       <div className="header-top">
@@ -24,8 +20,6 @@ function App() {  //used prop.clickedDorm[] to get clicked dorm
       <div className="content">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Mapbox filteredDorms={filteredDorms} setFilteredDorms={setFilteredDorms}
-            clickedDorm={clickedDorm} setClickedDorm={setClickedDorms}></Mapbox>
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={"Contact Page"} />
         </Routes>
--- front-end/src/Home.tsx
+++ front-end/src/Home.tsx
@@ -4,17 +4,20 @@ import { SetStateAction, useState } from 'react'
 import './styles/App.css'
 import './styles/Home.css'
 import Dropdowns from './Dropdowns'
-import MapBox from './MapBox'
+import MapBox from './Map/MapBox'
 import StandardDorm from './StandardDorm'
 import Suite, { DormRoomProps } from './Suite'
 
 function Home() {
-  const [count, setCount] = useState(0)
   const [resultString, setResultString] = useState('');
   const [building, setBuilding] = useState('');
+  //used prop.clickedDorm[] to get clicked dorm
+  const [filteredDorms, setFilteredDorms] = useState<Array<string>>([]);
+  const [clickedDorm, setClickedDorms] = useState<string>('');
+  //set filtered dorms to change map highlights.
 
-  const suiteRoom1: DormRoomProps = {roomNumber: "BARBOUR 080 081", roomType: "Double", roomSize: 228}
-  const suiteRoom2: DormRoomProps = {roomNumber: "BARBOUR 080 084", roomType: "Double", roomSize: 199}
+  const suiteRoom1: DormRoomProps = { roomNumber: "BARBOUR 080 081", roomType: "Double", roomSize: 228 }
+  const suiteRoom2: DormRoomProps = { roomNumber: "BARBOUR 080 084", roomType: "Double", roomSize: 199 }
   const suiteRooms = [suiteRoom1, suiteRoom2]
 
 
@@ -22,32 +25,33 @@ function Home() {
     <>
       <div>
         <div className="header-bottom">
-          <Dropdowns resultStr={resultString} setResultStr={setResultString}></Dropdowns> 
+          <Dropdowns resultStr={resultString} setResultStr={setResultString}></Dropdowns>
         </div>
         <div className="panel-container">
-            <div className="panel left">{resultString}</div>
-            <div className="panel center"><MapBox></MapBox></div>
-            <div className="panel right">
-                <StandardDorm 
-                    roomNumber={'BARBOUR 106'} 
-                    floorPlanLink={'https://brown.edu/Facilities/Facilities_Management/maps/docx/floor_plan/m_100129_1_dwg_base_a_8x11l.pdf'} 
-                    roomType={'Double'} 
-                    bathroomType={'Private'} 
-                    kitchenType={'No'} 
-                    roomSize={379}
-                    building="BARBOUR">
-                </StandardDorm>
-                <Suite
-                  suiteNumber={'BARBOUR 080'}
-                  floorPlanLink={'https://brown.edu/Facilities/Facilities_Management/maps/docx/floor_plan/m_100129_0_dwg_base_a_8x11l.pdf'}
-                  capacity={4}
-                  bathroom={'Private'}
-                  kitchen={'Yes'}
-                  commonAreaSize={385}
-                  dormRooms={suiteRooms}>
-                </Suite>
-            </div>
-         </div>
+          <div className="panel left">{resultString}</div>
+          <div className="panel center"><MapBox filteredDorms={filteredDorms} setFilteredDorms={setFilteredDorms}
+            clickedDorm={clickedDorm} setClickedDorm={setClickedDorms}></MapBox></div>
+          <div className="panel right">
+            <StandardDorm
+              roomNumber={'BARBOUR 106'}
+              floorPlanLink={'https://brown.edu/Facilities/Facilities_Management/maps/docx/floor_plan/m_100129_1_dwg_base_a_8x11l.pdf'}
+              roomType={'Double'}
+              bathroomType={'Private'}
+              kitchenType={'No'}
+              roomSize={379}
+              building="BARBOUR">
+            </StandardDorm>
+            <Suite
+              suiteNumber={'BARBOUR 080'}
+              floorPlanLink={'https://brown.edu/Facilities/Facilities_Management/maps/docx/floor_plan/m_100129_0_dwg_base_a_8x11l.pdf'}
+              capacity={4}
+              bathroom={'Private'}
+              kitchen={'Yes'}
+              commonAreaSize={385}
+              dormRooms={suiteRooms}>
+            </Suite>
+          </div>
+        </div>
       </div>
     </>
   )
