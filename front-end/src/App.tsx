495246d7155249914d77e0bc1b8557630b4e7b06
knewlin713 <kaley_newlin@brown.edu>
2024-12-19 07:19:47 -0800

    test files created and some playwrigth install

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,24 +7,47 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import About from './About'
 import Home from './Home'
 import Navbar from './Navbar'
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/clerk-react";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="header-top">
-        <div className="title">Brown Dorm Finder</div>
-        <Navbar />
+    <div><SignedOut>
+      <div className="container">
+        <h1 className="welcome-text">Welcome!</h1>
+        <p className="subtitle">We are all about yes</p>
+        <SignInButton className="cta-button">Sign In</SignInButton>
       </div>
-
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={"Contact Page"} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    </SignedOut><SignedIn>
+        <div>
+          <BrowserRouter>
+            <div className="header-top">
+              <div className="title">Brown Dorm Finder</div>
+              <Navbar />
+            </div>
+            <div className="content">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={"Contact Page"} />
+              </Routes>
+              <SignOutButton aria-label="SignOut" />
+            </div>
+          </BrowserRouter>
+        </div>
+      </SignedIn></div>
   )
 }
 
